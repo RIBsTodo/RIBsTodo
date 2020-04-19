3041baec394af4b6173e409d875da257e960ff8d
손명기 <audrl1010@naver.com>
2020-04-19 17:01:13 +0900

    Create TaskServiceSpec.swift

---
+++ RIBsTodoTests/Sources/Services/TaskServiceSpec.swift
@@ -0,0 +1,18 @@
+//
+//  TaskServiceSpec.swift
+//  RIBsTodoTests
+//
+//  Created by myung gi son on 2020/04/19.
+//  Copyright © 2020 myunggison. All rights reserved.
+//
+
+import RxSwift
+import Quick
+import Nimble
+import Stubber
+
+@testable import RIBsTodo
+
+final class TaskServiceSpec: QuickSpec {
+  override func spec() {}
+}
