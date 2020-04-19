1036bf13a52f272c6a8f4f7e41c4106c49ad0cbb
손명기 <audrl1010@naver.com>
2020-04-19 17:00:24 +0900

    Create TaskTable.swift

---
+++ RIBsTodo/Sources/Models/Realm/TaskTable.swift
@@ -0,0 +1,31 @@
+//
+//  TaskTable.swift
+//  RIBsTodo
+//
+//  Created by myung gi son on 2020/04/19.
+//  Copyright © 2020 myunggison. All rights reserved.
+//
+
+import RealmSwift
+
+@objcMembers class TaskTable: Object {
+  
+  enum Property: String {
+    case id, title, memo, isChecked, createdAt, updateAt
+  }
+  
+  dynamic var id: String = UUID().uuidString
+  dynamic var title: String = ""
+  dynamic var memo: String = ""
+  dynamic var isChecked: Bool = false
+  dynamic var createdAt: Date = Date()
+  dynamic var updateAt: Date = Date()
+  
+  override static func primaryKey() -> String? {
+    return TaskTable.Property.id.rawValue
+  }
+  
+  override class func indexedProperties() -> [String] {
+    return [TaskTable.Property.updateAt.rawValue]
+  }
+}
