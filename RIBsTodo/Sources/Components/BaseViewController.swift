c3be855b365c9863f29f8cafd4a553dfc864d441
손명기 <audrl1010@naver.com>
2020-04-19 16:59:35 +0900

    Create BaseViewController.swift

---
+++ RIBsTodo/Sources/Components/BaseViewController.swift
@@ -0,0 +1,14 @@
+//
+//  BaseViewController.swift
+//  RIBsTodo
+//
+//  Created by myung gi son on 2020/04/19.
+//  Copyright © 2020 myunggison. All rights reserved.
+//
+
+import RxSwift
+import UIKit
+
+class BaseViewController: UIViewController {
+  var disposeBag = DisposeBag()
+}
