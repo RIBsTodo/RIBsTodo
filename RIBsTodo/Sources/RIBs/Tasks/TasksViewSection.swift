dc10ab326fc6a37eb366b583d6784bafe3ab55bb
손명기 <audrl1010@naver.com>
2020-04-19 17:01:09 +0900

    Create TasksViewSection.swift

---
+++ RIBsTodo/Sources/RIBs/Tasks/TasksViewSection.swift
@@ -0,0 +1,35 @@
+//
+//  TasksViewSection.swift
+//  RIBsTodo
+//
+//  Created by myung gi son on 2020/04/19.
+//  Copyright © 2020 myunggison. All rights reserved.
+//
+
+import RxDataSources
+
+struct TasksViewSection: Equatable {
+  enum Identity: String {
+    case tasks
+  }
+  let identity: Identity
+  let items: [Item]
+}
+
+extension TasksViewSection: AnimatableSectionModelType {
+  init(original: TasksViewSection, items: [Item]) {
+    self = TasksViewSection(identity: original.identity, items: items)
+  }
+}
+
+extension TasksViewSection {
+  enum Item: Hashable {
+    case task(TaskCellReactor)
+  }
+}
+
+extension TasksViewSection.Item: IdentifiableType {
+  var identity: String {
+    return "\(self.hashValue)"
+  }
+}
