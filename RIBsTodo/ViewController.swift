9b737c6c50ceb80c3a8ebbae4645563ae367ace8
손명기 <audrl1010@naver.com>
2020-04-19 12:26:58 +0900

    Initial Commit

---
+++ RIBsTodo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RIBsTodo
+//
+//  Created by myung gi son on 2020/04/19.
+//  Copyright © 2020 myunggison. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
