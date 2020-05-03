005000e6f46cebad1f68e4b554610698d0f8265c
손명기 <audrl1010@naver.com>
2020-05-04 02:39:34 +0900

    Update TaskService.swift

--- RIBsTodo/Sources/Services/TaskService.swift
+++ RIBsTodo/Sources/Services/TaskService.swift
@@ -12,40 +12,76 @@ import RealmSwift
 import RxRealm
 
 protocol TaskServiceProtocol {
-  var event: PublishRelay<Task.Event> { get set }
-  func create(title: String, memo: String) -> Single<Task>
+  var event: PublishRelay<Task.Event> { get }
+  
+  func update(id: String, title: String, memo: String) -> Single<Void>
+  func create(title: String, memo: String) -> Single<Void>
+  func delete(id: String) -> Single<Void>
   func tasks() -> Single<[Task]>
+  func mark(id: String) -> Single<Void>
+  func unmark(id: String) -> Single<Void>
+  func move(id: String, destinationIndex: Int) -> Single<Void>
 }
 
 class TaskService: TaskServiceProtocol {
+  
   var event: PublishRelay<Task.Event> = .init()
   
-  private let realm: Realm
+  private let repository: TaskRepositoryProtocol
   
-  init(realm: Realm) {
-    self.realm = realm
+  init(repository: TaskRepositoryProtocol) {
+    self.repository = repository
   }
   
-  func create(title: String, memo: String) -> Single<Task> {
-    return Single.create { observer in
-      do {
-        try self.realm.write {
-          let taskTable = TaskTable(title: title, memo: memo)
-          self.realm.add(taskTable)
-          observer(.success(Task(taskTable)))
-        }
-      } catch let error {
-        observer(.error(error))
-      }
-      return Disposables.create()
-    }
+  func delete(id: String) -> Single<Void> {
+    return self.repository.delete(id: id)
+      .do(onSuccess: { task in
+        self.event.accept(Task.Event.delete(id: task.id))
+      })
+      .map { _ in }
+  }
+  
+  func create(title: String, memo: String) -> Single<Void> {
+    return self.repository.create(title: title, memo: memo)
+      .do(onSuccess: { task in
+        self.event.accept(Task.Event.create(task))
+      })
+      .map { _ in }
   }
   
   func tasks() -> Single<[Task]> {
-    return Single.create { observer in
-      let tasks = Array(self.realm.objects(TaskTable.self).map(Task.init))
-      observer(.success(tasks))
-      return Disposables.create()
-    }
+    return self.repository.tasks()
+  }
+  
+  func mark(id: String) -> Single<Void> {
+    return self.repository.mark(id: id)
+    .do(onSuccess: { task in
+      self.event.accept(Task.Event.mark(id: id))
+    })
+    .map { _ in }
+  }
+  
+  func unmark(id: String) -> Single<Void> {
+    return self.repository.unmark(id: id)
+    .do(onSuccess: { task in
+      self.event.accept(Task.Event.unmark(id: id))
+    })
+    .map { _ in }
+  }
+  
+  func move(id: String, destinationIndex: Int) -> Single<Void> {
+    return self.repository.move(id: id, destinationIndex: destinationIndex)
+    .do(onSuccess: { task in
+      self.event.accept(Task.Event.move(id: task.id, destinationIndex: destinationIndex))
+    })
+    .map { _ in }
+  }
+  
+  func update(id: String, title: String, memo: String) -> Single<Void> {
+    return self.repository.update(id: id, title: title, memo: memo)
+    .do(onSuccess: { task in
+      self.event.accept(Task.Event.update(task))
+    })
+    .map { _ in }
   }
 }
