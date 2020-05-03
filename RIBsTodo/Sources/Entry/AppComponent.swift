ad74dbe1c11feaf2d297ccc3b1a8ed635d3281b2
손명기 <audrl1010@naver.com>
2020-05-04 02:40:07 +0900

    set up di container

--- RIBsTodo/Sources/Entry/AppComponent.swift
+++ RIBsTodo/Sources/Entry/AppComponent.swift
@@ -7,8 +7,30 @@
 //
 
 import RIBs
+import Swinject
+import RealmSwift
+
+private let dependencyInjectionContainer = Container()
+
+extension Component {
+  var container: Container { return dependencyInjectionContainer }
+}
+
 class AppComponent: Component<EmptyDependency>, RootDependency {
+  
   init() {
     super.init(dependency: EmptyComponent())
+    self.container.register(Realm.self) { r in
+      try! Realm()
+    }.inObjectScope(.container)
+    self.container.register(TaskRepositoryProtocol.self) { r in
+      TaskRealmRepository(realm: r.resolve(Realm.self)!)
+    }.inObjectScope(.container)
+    self.container.register(TaskServiceProtocol.self) { r in
+      TaskService(repository: r.resolve(TaskRepositoryProtocol.self)!)
+    }.inObjectScope(.container)
+    self.container.register(AlertServiceProtocol.self) { r in
+      AlertService()
+    }.inObjectScope(.container)
   }
 }
