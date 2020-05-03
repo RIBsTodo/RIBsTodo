7b6e76b10d57aa4fe3e8027630e4ee6c0a176f64
손명기 <audrl1010@naver.com>
2020-05-04 02:58:59 +0900

    delete prepareForReuse() logic

--- RIBsTodo/Sources/Components/BaseTableCell.swift
+++ RIBsTodo/Sources/Components/BaseTableCell.swift
@@ -22,11 +22,6 @@ class BaseTableViewCell: UITableViewCell {
   required init?(coder aDecoder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
- 
-  override func prepareForReuse() {
-    super.prepareForReuse()
-    self.disposeBag = DisposeBag()
-  }
-  
+
   func initialize() {}
 }
