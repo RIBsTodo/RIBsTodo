d07a036d6640ff26600fe7e8cab09498cb90bc46
손명기 <audrl1010@naver.com>
2020-05-04 02:38:57 +0900

    rename "TaskTable" -> "RMTask"

--- RIBsTodo/Sources/Models/Realm/RMTask.swift
+++ RIBsTodo/Sources/Models/Realm/RMTask.swift
@@ -1,5 +1,5 @@
 //
-//  TaskTable.swift
+//  RMTask.swift
 //  RIBsTodo
 //
 //  Created by myung gi son on 2020/04/19.
@@ -8,32 +8,43 @@
 
 import RealmSwift
 
-@objcMembers class TaskTable: Object {
+@objcMembers class RMTask: Object {
   
   enum Property: String {
-    case id, title, memo, isChecked, createdAt, updateAt
+    case id, title, memo, isMarked, createdAt, updateAt
   }
   
   dynamic var id: String = UUID().uuidString
   dynamic var title: String = ""
   dynamic var memo: String = ""
-  dynamic var isChecked: Bool = false
+  dynamic var isMarked: Bool = false
   dynamic var createdAt: Date = Date()
   dynamic var updateAt: Date = Date()
   
   override static func primaryKey() -> String? {
-    return TaskTable.Property.id.rawValue
+    return Property.id.rawValue
   }
   
   override class func indexedProperties() -> [String] {
-    return [TaskTable.Property.updateAt.rawValue]
+    return [Property.updateAt.rawValue]
   }
 }
 
-extension TaskTable {
+extension RMTask {
   convenience init(title: String, memo: String) {
     self.init()
     self.title = title
     self.memo = memo
+  }
+}
+
+extension Task {
+  init(_ task: RMTask) {
+    self.id = task.id
+    self.title = task.title
+    self.memo = task.memo
+    self.isMarked = task.isMarked
+    self.createdAt = task.createdAt
+    self.updateAt = task.updateAt
   }
 }
