fdde3bb92f292a99b89a638bd545c816c384db62
손명기 <audrl1010@naver.com>
2020-05-05 13:57:26 +0900

    Update TaskServiceStub.swift

--- RIBsTodoTests/Sources/Services/TaskServiceStub.swift
+++ RIBsTodoTests/Sources/Services/TaskServiceStub.swift
@@ -12,10 +12,41 @@ import Stubber
 
 @testable import RIBsTodo
 
-final class TaskService: TaskServiceProtocol {
+final class TaskServiceStub: TaskServiceProtocol {
+  
   var event: PublishRelay<Task.Event> = .init()
+  
+  let repository: TaskRepositoryProtocol
+  
+  init(repository: TaskRepositoryProtocol) {
+    self.repository = repository
+  }
+  
+  func delete(id: String) -> Single<Void> {
+    return Stubber.invoke(delete, args: (id), default: .never())
+  }
+  
+  func create(title: String, memo: String) -> Single<Void> {
+    return Stubber.invoke(create, args: (title, memo), default: .never())
+  }
   
   func tasks() -> Single<[Task]> {
     return Stubber.invoke(tasks, args: (), default: .never())
+  }
+  
+  func mark(id: String) -> Single<Void> {
+    return Stubber.invoke(mark, args: (id), default: .never())
+  }
+  
+  func unmark(id: String) -> Single<Void> {
+    return Stubber.invoke(unmark, args: (id), default: .never())
+  }
+  
+  func move(id: String, destinationIndex: Int) -> Single<Void> {
+    return Stubber.invoke(move, args: (id, destinationIndex), default: .never())
+  }
+  
+  func update(id: String, title: String, memo: String) -> Single<Void> {
+    return Stubber.invoke(update, args: (id, title, memo), default: .never())
   }
 }
