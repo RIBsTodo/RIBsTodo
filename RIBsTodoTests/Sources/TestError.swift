fac7f2bc4de020663f1bfaadb254148fe6649b06
손명기 <audrl1010@naver.com>
2020-05-05 13:57:21 +0900

    Create TestError.swift

---
+++ RIBsTodoTests/Sources/TestError.swift
@@ -0,0 +1,11 @@
+//
+//  TestError.swift
+//  RIBsTodoTests
+//
+//  Created by myung gi son on 2020/05/04.
+//  Copyright © 2020 myunggison. All rights reserved.
+//
+
+import Foundation
+
+struct TestError: Error {}
