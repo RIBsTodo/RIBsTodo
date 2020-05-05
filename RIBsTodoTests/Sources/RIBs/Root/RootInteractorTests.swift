577f191511450cc0fbe6dd1743bb60508294e402
손명기 <audrl1010@naver.com>
2020-05-05 14:04:22 +0900

    correct folder path

---
+++ RIBsTodoTests/Sources/RIBs/Root/RootInteractorTests.swift
@@ -0,0 +1,30 @@
+//
+//  RootInteractorTests.swift
+//  RIBsTodoTests
+//
+//  Created by myung gi son on 2020/05/04.
+//  Copyright © 2020 myunggison. All rights reserved.
+//
+
+@testable import RIBsTodo
+import XCTest
+
+final class RootInteractorTests: XCTestCase {
+  
+  private var interactor: RootInteractor!
+  
+  // TODO: declare other objects and mocks you need as private vars
+  
+  override func setUp() {
+    super.setUp()
+    
+    // TODO: instantiate objects and mocks
+  }
+  
+  // MARK: - Tests
+  
+  func test_exampleObservable_callsRouterOrListener_exampleProtocol() {
+    // This is an example of an interactor test case.
+    // Test your interactor binds observables and sends messages to router or listener.
+  }
+}
---
+++ RIBsTodoTests/Sources/RIBs/Root/RootRouterTests.swift
@@ -0,0 +1,30 @@
+//
+//  RootRouterTests.swift
+//  RIBsTodoTests
+//
+//  Created by myung gi son on 2020/05/04.
+//  Copyright © 2020 myunggison. All rights reserved.
+//
+
+@testable import RIBsTodo
+import XCTest
+
+final class RootRouterTests: XCTestCase {
+  
+  private var router: RootRouter!
+  
+  // TODO: declare other objects and mocks you need as private vars
+  
+  override func setUp() {
+    super.setUp()
+    
+    // TODO: instantiate objects and mocks
+  }
+  
+  // MARK: - Tests
+  
+  func test_routeToExample_invokesToExampleResult() {
+    // This is an example of a router test case.
+    // Test your router functions invokes the corresponding builder, attachesChild, presents VC, etc.
+  }
+}
